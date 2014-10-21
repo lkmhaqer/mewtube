d3d8a37877a8376ad479bc6850dcc54a60ae98e7
lkmhaqer <cvnweb+github@gmail.com>
2014-10-21 07:53:25 -0500

    Delete searchtest.php
    not necessary

--- searchtest.php
+++
@@ -1,9 +0,0 @@
-<?php
-$query = "SELECT episodes.name, episodes.filename
-FROM ewwtube.episodes
-WHERE name LIKE '%S04%'
-UNION ALL
-SELECT movies.title, movies.filename
-FROM ewwtube.movies
-WHERE title LIKE '%Zoo%'";
-?>
