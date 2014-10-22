42f4af09577751c5f7617a45da0dd1bd0c15a891
lkmhaqer <cvnweb+github@gmail.com>
2014-10-22 06:35:41 -0500

    Delete notes.inc.php
    not needed.

--- notes.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-/*
-* index.php touched a bit, using a bit of the db() class, kinda want to use the pageBuilder() (after its extended a bit) class for it.
-* 
-* dbinfo.php has all classes (maybe this should change?)
-* advfile.php is the backend
-* 
-* vid.php/stats.php and other random shit should be rewritten into advfile, and then the javascrupt will have to be redone.
-*
